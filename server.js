3753a788bc8d22ae6c547f8199642a1b953d2d13
Janet <janet.cw.lai@tw.ey.com>
2025-08-15 18:01:21 +0800

    update

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/webhook/actionCompleted', (req, res) => {
+app.post('/', (req, res) => {
   console.log('收到 Webhook:', req.body);
   res.sendStatus(200);
 });
